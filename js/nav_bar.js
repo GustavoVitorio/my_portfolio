be405b0a7057b32e7ab1b277d9a3d476627b98a1
Gustavo <gustavovitorio07@gmail.com>
2026-05-05 22:10:02 -0300

    fit: adicionou o page about, feito algumas alterações no index.html, Home.css e style.css, além de adicionar icons e imagens necessarios á page about

---
+++ js/nav_bar.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('nav_bar');
+const menu = document.querySelector('nav');
+const seta = document.querySelector('#nav_bar img')
+
+button.addEventListener('click', ( ) => {
+    menu.classList.toggle('active');
+    seta.classList.toggle('rotate');
+});
