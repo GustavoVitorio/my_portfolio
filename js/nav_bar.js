bbe5fb9f868bd1ca67ad7f972b4f1aca1de86a49
Gustavo <gustavovitorio07@gmail.com>
2026-05-11 19:38:14 -0300

    style: feito todo o stylesheet e responsividade

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -2,7 +2,7 @@ const button = document.getElementById('nav_bar');
 const menu = document.querySelector('nav');
 const seta = document.querySelector('#nav_bar img')
 
-button.addEventListener('click', ( ) => {
-    menu.classList.toggle('active');
+button.addEventListener('click', () => {
+    menu.classList.toggle('expand');
     seta.classList.toggle('rotate');
 });
